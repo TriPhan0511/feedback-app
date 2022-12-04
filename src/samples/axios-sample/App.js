d54e603f988611b36fd4a44205964ae06914b2ef
Tri Phan <phanphutri@gmail.com>
2022-12-04 21:48:16 +0700

    Updated

--- src/samples/axios-sample/App.js
+++ src/samples/axios-sample/App.js
@@ -3,54 +3,116 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Post from './components/Post'
 
-// Make A Post Request
+// // Make A Delete Request
+// function App() {
+//   const [posts, setPosts] = useState([])
+
+//   useEffect(() => {
+//     const fetchPosts = async () => {
+//       const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+//       setPosts(response.data)
+//     }
+//     fetchPosts()
+//   }, [])
+
+//   const deletePost = async (id) => {
+//     await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//     setPosts(posts.filter((post) => post.id !== id))
+//     alert('A post was deleted.')
+//   }
+
+//   // const deletePost = (id) => {
+//   //   axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`).then(() => {
+//   //     alert('A post was deleted.')
+//   //     setPosts(posts.filter((post) => post.id !== id))
+//   //   })
+//   // }
+
+//   if (!posts) {
+//     return <div>Loading...</div>
+//   }
+//   return (
+//     <>
+//       {posts.slice(0, 5).map((post) => (
+//         <Post post={post} deletePost={deletePost} />
+//       ))}
+//     </>
+//   )
+// }
+
+// --------------------------------------------------------------------------------
+// // Make A Put Request
+// // (Update a post)
+// function App() {
+//   const [posts, setPosts] = useState([])
+//   const [post, setPost] = useState(null)
+//   const [title, setTitle] = useState('')
+//   const [body, setBody] = useState('')
+//   useEffect(() => {
+//     const fetchPosts = async () => {
+//       const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+//       setPosts(response.data)
+//     }
+//     fetchPosts()
+//   }, [])
+
+//   const updatePost = async (id) => {
+//     const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+//       title: 'new title',
+//       body: 'new body',
+//     })
+//     setPost()
+//   }
+
+//   if (!posts) {
+//     return <p>Loading...</p>
+//   }
+//   return (
+//     <>
+//       {posts.map((post) => (
+//         <Post key={post.id} post={post} updatePost={updatePost} />
+//       ))}
+//     </>
+//   )
+// }
+// -------------------------------------------------------
 function App() {
   const [post, setPost] = useState(null)
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
-  // Get a specific post
   useEffect(() => {
     const fetchPost = async () => {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts/99')
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/1`)
       setPost(response.data)
     }
     fetchPost()
   }, [])
-  // Create a new post
-  const createPost = async () => {
-    const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-      title,
-      body,
+
+  const updatePost = async (id) => {
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/1`, {
+      title: 'new title',
+      body: 'new body',
     })
     setPost(response.data)
   }
+
   if (!post) {
     return <p>Loading...</p>
   }
   return (
     <>
-      <Post post={post} />
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          createPost()
-        }}
-      >
-        <label htmlFor='title'>Title</label>
-        <input type='text' id='title' name={title} onChange={(e) => setTitle(e.target.value)} />
-        <br />
-        <label htmlFor='body'>Body</label>
-        <input type='text' id='body' name={body} onChange={(e) => setBody(e.target.value)} /> <br />
-        <button type='submit'>Submit</button>
-      </form>
+      <Post post={post} updatePost={updatePost} />
     </>
   )
 }
 
 // --------------------------------------------------------------------------------
-// // Make A Post Request
+// // Make A POST Request
 // function App() {
 //   const [post, setPost] = useState(null)
+//   const [title, setTitle] = useState('')
+//   const [body, setBody] = useState('')
+
 //   useEffect(() => {
 //     const fetchPost = async () => {
 //       const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
@@ -58,28 +120,33 @@ function App() {
 //     }
 //     fetchPost()
 //   }, [])
-//   const createPost = async () => {
+
+//   const createPost = async (title, body) => {
 //     const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-//       title: 'Have a nice weekend',
-//       body: `Let's go out!`,
+//       title,
+//       body,
 //     })
 //     setPost(response.data)
 //   }
-//   // function createPost() {
-//   //   axios
-//   //     .post('https://jsonplaceholder.typicode.com/posts', {
-//   //       title: 'Hello World!',
-//   //       body: 'This is a new post.',
-//   //     })
-//   //     .then((response) => setPost(response.data))
-//   // }
+
 //   if (!post) {
-//     return <p>Loading...</p>
+//     return <div>Loading...</div>
 //   }
 //   return (
 //     <>
 //       <Post post={post} />
-//       <button onClick={createPost}>Create Post</button>
+//       <form
+//         onSubmit={(e) => {
+//           e.preventDefault()
+//           createPost(title, body)
+//         }}
+//       >
+//         <label htmlFor='title'>Title</label>
+//         <input type='text' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+//         <label htmlFor='body'>Body</label>
+//         <input type='text' id='body' value={body} onChange={(e) => setBody(e.target.value)} />
+//         <button type='submit'>Submit</button>
+//       </form>
 //     </>
 //   )
 // }
@@ -88,18 +155,24 @@ function App() {
 // // Get An Individual Post
 // function App() {
 //   const [post, setPost] = useState(null)
+//   const [error, setError] = useState(null)
+
 //   useEffect(() => {
 //     const fetchPost = async () => {
-//       const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
-//       setPost(response.data)
+//       try {
+//         const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+//         // const response = await axios.get('https://jsonplaceholder.typicode.com/posts/10000')
+//         setPost(response.data)
+//       } catch (error) {
+//         setError(error)
+//       }
 //     }
 //     fetchPost()
 //   }, [])
-//   // useEffect(() => {
-//   //   axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
-//   //     setPost(response.data)
-//   //   })
-//   // }, [])
+
+//   if (error) {
+//     return <div>{error.message}</div>
+//   }
 //   if (!post) {
 //     return <p>Loading...</p>
 //   }
--- src/samples/axios-sample/components/Post.js
+++ src/samples/axios-sample/components/Post.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-function Post({ post: { id, title, body } }) {
+function Post({ post: { id, title, body }, deletePost, upDatePost }) {
   return (
-    <div>
+    <div style={{ fontSize: 20 }}>
       <h3>Id: {id}</h3>
-      <p style={{ fontSize: 20 }}>Title: {title}</p>
-      {body && <p style={{ fontSize: 20 }}>Body: {body}</p>}
+      <div>
+        <strong>Title:</strong> {title}
+      </div>
+      <div>
+        <strong>Body:</strong> {body}
+      </div>
+      <button onClick={() => upDatePost(id)}>Update</button>
+      {/* <button onClick={() => deletePost(id)}>Delete</button> */}
     </div>
   )
 }
